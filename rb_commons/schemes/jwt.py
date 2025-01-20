433eceee84ae1fb724eb1d0f781670676c23c7d5
abdulvoris <erkinovabdulvoris101@gmail.com>
2025-01-20 17:24:15 +0500

    fix: fixed claims

--- rb_commons/schemes/jwt.py
+++ rb_commons/schemes/jwt.py
@@ -1,6 +1,6 @@
 import uuid
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class UserRole(str, Enum):
@@ -9,9 +9,8 @@ class UserRole(str, Enum):
     GUEST = "guest"
 
 class Claims(BaseModel):
+    model_config = ConfigDict(extra="ignore")
+
     user_id: str = Field(None, alias="x-user-id")
     user_role: UserRole = Field(UserRole.GUEST, alias="x-user-role")
     shop_id: uuid.UUID = Field(None, alias="x-shop-id")
-
-    class Config:
-        extra = "ignore"
