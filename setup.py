0c0d52ed7f69474bb2673f34bbc5ea9e354bbbf6
Abdulvoris <erkinovabdulvoris101@gmail.com>
2026-03-14 13:09:41 +0500

    feat: added new py jwt requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="rb-commons",
-    version="0.7.27",
+    version="0.7.28",
     author="Abdulvoris",
     author_email="erkinovabdulvoris101@gmail.com",
     description="Commons of project and simplified orm based on sqlalchemy.",
@@ -23,7 +23,7 @@ setup(
         "annotated-types==0.7.0",
         "greenlet==3.1.1",
         "pydantic>=1.7.4,<3.0.0",
-        "PyJWT==2.10.1",
+        "PyJWT>=2.12.0",
         "python-dotenv==1.0.1",
         "SQLAlchemy==2.0.36",
         "fastapi>=0.115.6,<0.120.0",
